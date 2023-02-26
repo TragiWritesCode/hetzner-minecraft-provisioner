6f6ce606a1acdbc935fee312284a73478a7c64b6
stefan <spam.stefan@gmx.de>
2023-02-26 12:27:53 +0100

    fixed formatting, added volume to server createion script
    Signed-off-by: stefan <spam.stefan@gmx.de>

--- src/shell/hetzner/create-server.sh
+++ src/shell/hetzner/create-server.sh
@@ -9,4 +9,6 @@
 #5    ash-dc1     Ashburn DC1          ash
 #6    hil-dc1     Hillsboro DC 1       hil
 
-hcloud server create --name mc-luki-1 --without-ipv6 --datacenter 2 --firewall fw-minecraft --image debian-11 --type cx21 --ssh-key fedora-dev-vm --primary-ipv6 primary-ipv4-1
+hcloud server create --name mc-luki-1 --without-ipv6 \
+--datacenter 2 --firewall fw-minecraft --image debian-11 --type cx21 \
+--ssh-key fedora-dev-vm --primary-ipv6 primary-ipv4-1 --volume volume-nbg1-1
